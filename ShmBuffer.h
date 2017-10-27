1754b615e949a1ff5447221ca51134c1d0097021
William W Wold <wilcoze@gmail.com>
2017-10-26 20:25:53 -0700

    made header-only

--- ShmBuffer.h
+++ ShmBuffer.h
@@ -1,29 +1,92 @@
 #include <string>
+#include <iostream>
+#include <functional>
 #include <boost/interprocess/shared_memory_object.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
-#include <functional>
+
+#define debug_off(msg)
+#define debug_on(msg) std::cout << msg << std::endl
+
+// switch between debug_on and debug_off to toggle debugging
+#define debug debug_on
+
+using namespace boost::interprocess;
 
 class ShmBuffer
 {
 public:
-	ShmBuffer();
-	~ShmBuffer();
+	ShmBuffer()
+	{}
+	
+	~ShmBuffer()
+	{
+		if (isOpen)
+			close();
+	}
 	
 	// reserves a block of the given size that will be available at the returned offset once open is called
 	// this should not be called after open
 	// essentially all this does is increase the total size by the given size and returns the previous total size
-	int addBlock(int size);
+	int addBlock(int sizeIn)
+	{
+		if (isOpen)
+		{
+			throw std::runtime_error("ShmBuffer::addBlock() called after buffer was opened\n");
+		}
+		int offset = size;
+		size += sizeIn;
+		return offset;
+	}
 	
 	// opens the SHM buffer
-	void open(std::string name);
+	void open(std::string nameIn)
+	{
+		if (isOpen)
+		{
+			close();
+		}
+		name = nameIn;
+		try
+		{
+			debug("trying to open SHM");
+			shm = shared_memory_object(open_only, name.c_str(), read_write);
+		}
+		catch (boost::interprocess::interprocess_exception ex)
+		{
+			debug("creating SHM because open failed");
+			shm = shared_memory_object(create_only, name.c_str(), read_write);
+			shm.truncate(size);
+		}
+		region = mapped_region(shm, read_write);
+		isOpen = true;
+	}
 	
 	// must be called after opened, returns a pointer to the data
-	void * getData();
+	void * getData()
+	{
+		if (!isOpen)
+		{
+			throw std::runtime_error("ShmBuffer::getData() called before buffer was opened\n");
+		}
+		return region.get_address();
+	}
 	
 	// closes the SHM buffer, called automatically by the destructor if not called explicitly before
-	void close();
+	void close()
+	{
+		debug("closing SHM");
+		if (!isOpen)
+		{
+			throw std::runtime_error("ShmBuffer::close() called but buffer was not open\n");
+		}
+		region = mapped_region();
+		shm = shared_memory_object();
+		shared_memory_object::remove(name.c_str());
+		size = 0;
+		isOpen = false;
+	}
 	
 	bool getIsOpen() { return isOpen; }
 	
